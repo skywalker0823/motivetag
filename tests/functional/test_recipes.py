0b414efeb2344dbe14fe7f7401a082c07daea8d1
pikxl <skywalker0823@hotmail.com>
2022-06-07 18:33:59 +0800

    pytest testing

--- tests/functional/test_recipes.py
+++ tests/functional/test_recipes.py
@@ -7,13 +7,33 @@ def test_home_page():
     WHEN the '/' page is requested (GET)
     THEN check that the response is valid
     """
-    flask_app = create_app('flask_test.cfg')
+    flask_app = create_app('dev')
 
     # Create a test client using the Flask application configured for testing
     with flask_app.test_client() as test_client:
         response = test_client.get('/')
         assert response.status_code == 200
-        assert b"Welcome to the" in response.data
-        assert b"Flask User Management Example!" in response.data
-        assert b"Need an account?" in response.data
-        assert b"Existing user?" in response.data
+        # assert b"index.html" in response.data
+
+
+def test_home_page_post():
+    """
+    GIVEN a Flask application configured for testing
+    WHEN the '/' page is is posted to (POST)
+    THEN check that a '405' status code is returned
+    """
+    flask_app = create_app('dev')
+
+    # Create a test client using the Flask application configured for testing
+    with flask_app.test_client() as test_client:
+        response = test_client.post('/')
+        assert response.status_code == 405
+        # assert b"Flask User Management Example!" not in response.data
+
+
+# def test_block():
+#     flask_app = create_app('dev')
+#     with flask_app.test_client() as test_client:
+
+#         response = test_client.get('/api/blocks')
+#         assert response["error"] == "True"
--- tests/unit/test_models.py
+++ tests/unit/test_models.py
@@ -1,17 +1,25 @@
+#資料庫test
 
 from data.data import Member
-Member.sign_up
 
 
+
+
+def test_get_user():
+    """
+    GIVEN a User account
+    WHEN a User try to get data with account
+    THEN check should return user data account include
+    """
+    user = Member.get_member("123")
+    assert user["account"] == "123"
 
 
 def test_new_user():
     """
     GIVEN a User model
-    WHEN a new User is created
-    THEN check the email, hashed_password, and role fields are defined correctly
+    WHEN a User is created before
+    THEN check should return Already registed
     """
-    user = Member.sign_up("123", "123","123@123.com","1988-09-12","2022-01-01")
-    assert user.account == "123"
-    assert user.email == '123@123.COM'
-    assert user.hashed_password != '123'
+    user = Member.sign_up("123", "321","123@123.com","1988-09-12","2022-01-01")
+    assert user == "Already registed"
